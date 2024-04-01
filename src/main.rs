721f80f1db1e378afccc66ad73981ee5004a611b
Alexis <pinceminalexis@gmail.com>
2024-04-01 18:30:46 +0200

    Post a file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::io::Write;
 
 use clap::Parser;
 use clap::{arg, command};
@@ -8,10 +9,31 @@ use tokio::{
 };
 
 #[derive(Debug)]
+enum Method {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+}
+
+impl Method {
+    fn from_str(method: &str) -> Self {
+        match method {
+            "GET" => Method::GET,
+            "POST" => Method::POST,
+            "PUT" => Method::PUT,
+            "DELETE" => Method::DELETE,
+            _ => panic!("Unhandeled method: {}", method),
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Request {
-    method: String,
+    method: Method,
     path: Vec<String>,
     headers: HashMap<String, String>,
+    body: String,
 }
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -40,50 +62,78 @@ fn parse_request(buffer: &[u8]) -> anyhow::Result<Request> {
 
     let mut headers = HashMap::new();
     let request = request.trim();
-    let mut parts = request.split_whitespace();
+    let mut parts = request.split_ascii_whitespace();
 
-    let method = parts.next().unwrap_or_default().to_string();
+    let method = Method::from_str(parts.next().unwrap_or_default());
     let path = parts.next().unwrap_or_default().to_string();
 
     let path = path.splitn(3, '/').map(|s| s.to_string()).skip(1).collect();
 
     let user_agent = parts
+        .clone()
         .skip_while(|&s| s != "User-Agent:")
         .skip(1)
         .next()
         .unwrap_or_default()
         .to_string();
 
-    headers.insert("User-Agent".to_string(), user_agent);
+    headers.insert("User-Agent".to_string(), user_agent.clone());
+
+    let body = parts
+        .skip_while(|&s| s != "Content-Length:")
+        .skip(2)
+        .next()
+        .unwrap_or_default()
+        .to_string();
 
     Ok(Request {
         method,
         path,
         headers,
+        body,
     })
 }
 
-fn respond_with_content(content: &str, content_type: &str) -> String {
+fn response_with_content(content: &str, content_type: &str, code: u8) -> String {
     format!(
-        "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
+        "HTTP/1.1 {} OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
+        code,
         content_type,
         content.len(),
         content
     )
 }
 
-fn respond_with_404() -> String {
+fn response_with_404() -> String {
     "HTTP/1.1 404 NOT FOUND\r\n\r\n".to_string()
 }
 
-fn handle_files_request(directory: String, filename: &str) -> anyhow::Result<String> {
+fn handle_get_files_request(directory: String, filename: &str) -> anyhow::Result<String> {
     let path = std::path::Path::new(&directory).join(filename);
 
     let response = if path.exists() {
         let content = std::fs::read_to_string(path)?;
-        respond_with_content(&content, "application/octet-stream")
+        response_with_content(&content, "application/octet-stream", 200)
     } else {
-        respond_with_404()
+        response_with_404()
+    };
+
+    Ok(response)
+}
+
+fn handle_post_files_request(
+    directory: String,
+    filename: &str,
+    body: String,
+) -> anyhow::Result<String> {
+    let path = std::path::Path::new(&directory).join(filename);
+
+    let response = if path.exists() {
+        response_with_404()
+    } else {
+        let mut file = std::fs::File::create(path)?;
+        file.write_all(body.as_bytes())?;
+        response_with_content("Created", "text/plain", 201)
     };
 
     Ok(response)
@@ -94,34 +144,55 @@ async fn handle_connection(mut stream: TcpStream, directory: Option<String>) -> 
     let len = stream.read(&mut buffer).await?;
     let request = parse_request(&buffer[..len])?;
 
-    match request.method.as_str() {
-        "GET" => match request.path.get(0).unwrap().as_str() {
+    match request.method {
+        Method::GET => match request.path.get(0).unwrap().as_str() {
             "" => {
                 let response = format!("HTTP/1.1 200 OK\r\n\r\n");
                 stream.write_all(response.as_bytes()).await?;
             }
             "echo" => {
-                let response = respond_with_content(request.path.get(1).unwrap_or(&"".to_string()), "text/plain");
+                let response = response_with_content(
+                    request.path.get(1).unwrap_or(&"".to_string()),
+                    "text/plain",
+                    200,
+                );
                 stream.write_all(response.as_bytes()).await?;
             }
             "user-agent" => {
-                let response =
-                    respond_with_content(request.headers.get("User-Agent").unwrap(), "text/plain");
+                let response = response_with_content(
+                    request.headers.get("User-Agent").unwrap(),
+                    "text/plain",
+                    200,
+                );
                 stream.write_all(response.as_bytes()).await?;
             }
             "files" => {
                 let response = match directory {
-                    Some(directory) => handle_files_request(
+                    Some(directory) => handle_get_files_request(
                         directory,
                         request.path.get(1).unwrap_or(&"".to_string()),
                     )?,
-                    None => respond_with_404(),
+                    None => response_with_404(),
                 };
                 stream.write_all(response.as_bytes()).await?;
             }
-            _ => stream.write_all(respond_with_404().as_bytes()).await?,
+            _ => stream.write_all(response_with_404().as_bytes()).await?,
         },
-        _ => {}
+        Method::POST => match request.path.get(0).unwrap().as_str() {
+            "files" => {
+                let response = match directory {
+                    Some(directory) => handle_post_files_request(
+                        directory,
+                        request.path.get(1).unwrap_or(&"".to_string()),
+                        request.body,
+                    )?,
+                    None => response_with_404(),
+                };
+                stream.write_all(response.as_bytes()).await?;
+            }
+            _ => stream.write_all(response_with_404().as_bytes()).await?,
+        },
+        _ => stream.write_all(response_with_404().as_bytes()).await?,
     }
 
     Ok(())
